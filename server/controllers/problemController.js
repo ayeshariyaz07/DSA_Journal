7e800c09cd6273c0ed5712d7ae908008fece959c
ayeshariyaz07 <ayesha7riyaz@gmail.com>
2026-07-29 20:13:06 +0530

    feat: complete Day 3 backend with Gemini integration

--- server/controllers/problemController.js
+++ server/controllers/problemController.js
@@ -4,73 +4,121 @@ const { analyzeProblem } = require("../services/geminiService");
 /// Create Problem
 const createProblem = async (req, res) => {
   try {
+
     const {
       title,
       source,
-      problemLink,
       content,
-      myNotes,
+      problemLink,
+      myNotes
     } = req.body;
 
-    // Ask Gemini to analyze the problem
-    const aiResponse = await analyzeProblem(content);
+    if (!title || !source || !content) {
+      return res.status(400).json({
+        message: "Title, source and content are required."
+      });
+    }
 
-    // Save everything to MongoDB
+    const { pattern, reasoning } =
+      await analyzeProblem(content);
+
     const problem = await Problem.create({
       title,
       source,
       problemLink,
       content,
       myNotes,
-      pattern: aiResponse.pattern,
-      reasoning: aiResponse.reasoning,
+      pattern,
+      reasoning,
     });
 
     res.status(201).json(problem);
+
   } catch (error) {
-    console.error(error);
+
     res.status(500).json({
       message: error.message,
     });
+
   }
 };
 
 // Get All Problems
 const getProblems = async (req, res) => {
   try {
-    const problems = await Problem.find();
+    const { pattern } = req.query;
+
+    let query = {};
+
+    if (pattern) {
+      query.pattern = pattern;
+    }
+
+    const problems = await Problem.find(query);
+
     res.status(200).json(problems);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({
+      message: error.message,
+    });
   }
 };
 
 // Update Problem
 const updateProblem = async (req, res) => {
   try {
+
     const updated = await Problem.findByIdAndUpdate(
       req.params.id,
       req.body,
-      { new: true }
+      {
+        new: true,
+        runValidators: true,
+      }
     );
 
+    if (!updated) {
+      return res.status(404).json({
+        message: "Problem not found",
+      });
+    }
+
     res.status(200).json(updated);
+
   } catch (error) {
-    res.status(500).json({ message: error.message });
+
+    res.status(500).json({
+      message: error.message,
+    });
+
   }
 };
 
 // Delete Problem
 const deleteProblem = async (req, res) => {
+
   try {
-    await Problem.findByIdAndDelete(req.params.id);
+
+    const deleted = await Problem.findByIdAndDelete(req.params.id);
+
+    if (!deleted) {
+      return res.status(404).json({
+        message: "Problem not found",
+      });
+    }
 
     res.status(200).json({
       message: "Problem deleted successfully",
     });
+
   } catch (error) {
-    res.status(500).json({ message: error.message });
+
+    res.status(500).json({
+      message: error.message,
+    });
+
   }
+
 };
 
 module.exports = {
